3462d09ed45e7b9b43b441e75f51ace86d59a94b
filippohronsky <pohronsky@ditec.sk>
2025-06-01 12:41:02 +0200

    deleted:    .github/workflows/azure-static-web-apps-purple-pond-0583aad03.yml 	modified:   script.js

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-const totalImages = 10; // zmeň podľa počtu fotiek
-const interval = 3000;  // 5 sekúnd
-let current = 1;
+const imageList = [];
+let index = 1;
 
-function updateImage() {
-  const img = document.getElementById('photo');
-  img.src = `images/${current}.jpg`;
-  current = current % totalImages + 1;
+// Skontroluj existenciu obrázkov postupne
+function preloadImages(callback) {
+  function tryNext() {
+    const img = new Image();
+    img.onload = () => {
+      imageList.push(`${index}.jpg`);
+      index++;
+      tryNext();
+    };
+    img.onerror = () => {
+      callback(); // ukonči, keď už obrázok neexistuje
+    };
+    img.src = `images/${index}.jpg`;
+  }
+
+  tryNext();
+}
+
+function startCarousel(interval = 5000) {
+  let current = 0;
+
+  function updateImage() {
+    const img = document.getElementById('photo');
+    img.src = `images/${imageList[current]}`;
+    current = (current + 1) % imageList.length;
+  }
+
+  updateImage();
+  setInterval(updateImage, interval);
 }
 
 window.onload = () => {
-  updateImage();
-  setInterval(updateImage, interval);
+  preloadImages(() => {
+    if (imageList.length > 0) {
+      startCarousel(5000); // 5 sekúnd
+    } else {
+      console.error("No images found in /images/");
+    }
+  });
 };
